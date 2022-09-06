0d29bd75fa7371e7dccf74b4fe26303c22c10beb
Limon714 <limon4u714@gmail.com>
2022-09-07 01:39:38 +0600

    array

---
+++ qs2.php
@@ -0,0 +1,10 @@
+<?php 
+// Searching an array for a given value in PHP
+
+$findArray = array("Limon", "Mainul", "Mostafizur", " Bahar", 24);
+
+sort($findArray);
+
+print_r($findArray);
+
+?>
---
+++ qs3.php
@@ -0,0 +1,10 @@
+<?php 
+// how to Fill an array with values in PHP - Array Fill
+
+$findArray = array_fill(5, 4, array("Limon", 16, "Islam"));
+
+echo "<pre>";
+print_r($findArray);
+echo "</pre>";
+
+?>
