16c0f02e62fc7436bd243c3c35c05f90dd10891c
Limon714 <limon4u714@gmail.com>
2022-09-07 00:45:57 +0600

    php 1st question solve

---
+++ qs.php
@@ -0,0 +1,11 @@
+<?php 
+// How to find value from array in PHP
+
+$findArray = array("Limon", "Mainul", "Mostafizur", " Bahar", 24);
+
+if(in_array("Limon", $findArray)){
+    echo "<h1>We find Value</h1>";
+}else{
+    "We have to do in a better way";
+}
+?>
