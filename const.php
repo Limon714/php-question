24456a88c3cc6756e42da49e5a66989a9384568e
Limon714 <limon4u714@gmail.com>
2022-09-14 19:14:10 +0600

    constructor from Yahoo Baba

---
+++ const.php
@@ -0,0 +1,24 @@
+ <?php 
+
+class Person{
+    
+    public $name, $age;
+    function __construct($n= "AAA", $a = 23)
+    {
+            $this->name = $n;
+            $this->age = $a;
+    }
+    function show(){
+        echo $this->name . " is " . $this->age . "</br>";
+    }
+}
+$p = new Person();
+$p2 = new Person("Rabbil", 34);
+$p3 = new Person("Limon", 27);
+// $p->name = "Rabul";
+// $p->age = 24;
+
+$p->show();
+$p2->show();
+$p3->show();
+ ?>
